760b658cd2c4ca4d465a1f93c611703b8b0f0bcd
Erick Henrique <erick.souza.camargo@escola.pr.gov.br>
2026-09-09 11:30:43 -0300

    teste

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /tmp/guest-blhjwu/projetobiblioteca/projeto1biblioteca/venv
+set -gx VIRTUAL_ENV /tmp/guest-3cwouy/biblioteca_1B/projeto1biblioteca/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
